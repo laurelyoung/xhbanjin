1ab948d146a3c892cd134c51f2dcc8a771d84ba2
yangchenglong <903761074@qq.com>
2015-12-21 01:37:29 +0800

    改成面向对象的方式、修复滑动块拖拽功能

--- jquery-plugin/SliderBar.js
+++ jquery-plugin/SliderBar.js
@@ -6,39 +6,74 @@
  * 3.实现completed的实时更新
  * Created by laurel on 15/12/20.
  */
-(function ($) {
-    $.fn.SliderBar = function (options) {
+;(function ($) {
+    function SliderBar(element, options) {
+        this.$element = element;
+        this.default = {
+            renderTo: this.$element,//滑动条添加的位置
+            barClassName: 'bar',
+            completedClassName: 'completed',
+            sliderClassName: 'slider',
+            barWidth: 400,//滑动宽度
+            sliderWidth: 300,//滑块宽度
+            onChanging: function () {
+            },// 拖拽时触发的事件
+            onChanged: function () {
+            }//拖拽完成时触发的时间
+        };
         // 第一个参数为{},是为了避免破坏掉默认参数对象$.fn.SliderBar.default
-        var settings = $.extend({}, $.fn.SliderBar.default, options || {});
-        // 兼容jQuery对象和字符串.
-        // 如果是字符串,则将其转换成jQuery对象
-        settings.renderTo = ($.type(settings.renderTo) == 'string' ? $(settings.renderTo) : settings.renderTo);
-        console.log(settings);
+        this.options = $.extend({}, this.default, options);
+    };
 
-        var bar = $('<div></div>').attr('class', settings.barClassName).appendTo(settings.renderTo);
-        var completed = $('<div></div>').attr('class', settings.completedClassName).appendTo(bar);
-        var slider = $('<div></div>').attr('class', settings.sliderClassName).appendTo(bar);
+    SliderBar.prototype = {
+        init: function () {
+            // 兼容jQuery对象和字符串. 如果是字符串,则将其转换成jQuery对象
+            this.options.renderTo = ($.type(this.options.renderTo) == 'string' ? $(this.options.renderTo) : this.options.renderTo);
+            // 创建滑动条3个组成部分
+            var $bar = $('<div></div>').attr('class', this.options.barClassName).width(this.options.barWidth).appendTo(this.options.renderTo);
+            var $completed = $('<div></div>').attr('class', this.options.completedClassName).appendTo($bar);
+            var $slider = $('<div></div>').attr('class', this.options.sliderClassName).appendTo($bar);
 
-        var barWidth = bar.width();
-        var sliderWidth = bar.width();
+            var me = this;
 
-        console.log(this);
+            var sliderPageX = 0;// 鼠标在页面上的x轴坐标
+            var sliderLeft = 0;// 滑动块距离父元素左边距离
+            var enableDrag = false;// 控制滑块是否移动的标识
+            var min = 0, max = $bar.width() - $slider.width();// 滑动块的移动范围
+
+            document.onmousedown = function (e) {
+                e = e || window.event;
+                sliderLeft = parseInt($slider.css('left'));
+                sliderPageX = e.pageX;
+                enableDrag = true;
+                $slider.css('cursor', 'move');
+            };
+
+            document.onmousemove = function (e) {
+                e = e || window.event;
+                if (enableDrag) {
+                    // 给定滑动块的滑动区域(min ~ max),以免滑动滑到范围外
+                    var l = Math.min(Math.max(e.pageX - sliderPageX + sliderLeft, min), max);
+                    $slider.css('left', l + 'px');
+                    $completed.css('width', l + 'px');
+                    me.options.onChanging(l / max, e);
+                }
+            };
+
+            document.onmouseup = function (e) {
+                enableDrag = false;
+                me.options.onChanged(parseInt($slider.css('left')) / max, e);
+            };
+        }
+    };
+
+    $.fn.SliderBar = function (options) {
+        var sliderBar = new SliderBar(this, options);
+        sliderBar.init();
+        console.log(sliderBar);
         return this.each(function () {
 
         });
 
-    };
-
-    $.fn.SliderBar.default = {
-        renderTo: $(document.body),//滑动条添加的位置
-        barClassName: 'bar',
-        completedClassName: 'completed',
-        sliderClassName: 'slider',
-        barWidth: 400,//滑动宽度
-        sliderWidth: 300,//滑块宽度
-        onChanging: function () {
-        },// 拖拽时触发的事件
-        onChanged: function () {
-        }//拖拽完成时触发的时间
     };
 })(jQuery);
